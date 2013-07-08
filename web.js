174c5ba8f03ef3664942f8a630d3f9db58deca8b
xpresslanej <xpresslanej@gmail.com>
2013-07-08 06:08:45 +0000

    updated to read from index.html

--- web.js
+++ web.js
@@ -1,9 +1,11 @@
-var express = require('express');
+var express = require('express')
+   ,fs = require('fs');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+ var data = fs.readFileSync('./index.html').toString();  
+ response.send(data);
 });
 
 var port = process.env.PORT || 5000;
